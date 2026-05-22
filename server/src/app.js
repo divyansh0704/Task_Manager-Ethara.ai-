213b89ffe66125c4d2529b398627a0cf0ceabb90
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-05-23 02:31:55 +0530

    bug 5

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,20 @@ const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-app.use(cors(
-    {
-        origin: ["http://localhost:5173"],
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    }
-))
+// app.use(cors(
+//     {
+//         origin: ["http://localhost:5173"],
+//         credentials: true,
+//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//         allowedHeaders: ['Content-Type', 'Authorization']
+//     }
+// ))
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+};
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
