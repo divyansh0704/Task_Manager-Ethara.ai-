17f3cdfa6067fc4d099de482909db1c0067fb0d5
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-05-23 01:59:44 +0530

    cors position shifted to top

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,16 @@ const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser());
 app.use(cors(
     {
-        origin:["http://localhost:5173", "https://taskmanager-etharaai-production.up.railway.app"],
+        origin:["http://localhost:5173"],
         credentials:true
     }
 ))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+
 
 
 
