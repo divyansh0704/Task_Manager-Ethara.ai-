e8b20b0fe56e185239ece462f32063a2f53d568c
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-05-23 02:12:23 +0530

    other cors changes

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,16 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 app.use(cors(
     {
-        origin:["http://localhost:5173"],
-        credentials:true
+        origin: ["http://localhost:5173"],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
     }
 ))
-app.options('*', cors());
+
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.options('(.*)', cors());
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 
 
@@ -22,8 +25,8 @@ const projectRoutes = require("./routes/projectRoutes")
 const taskRoutes = require("./routes/taskRoutes")
 
 // using all routes here
-app.use("/api/auth",authRoutes)
-app.use("/api/project",projectRoutes)
-app.use("/api/task",taskRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/project", projectRoutes)
+app.use("/api/task", taskRoutes)
 
-module.exports=app;
+module.exports = app;
